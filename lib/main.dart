707daaf27019ff58881e93d409844a81a82448b5
AzureAD\고명민 <miung222@gmail.com>
2023-03-27 19:13:43 +0900

    Color 사전 설정

--- lib/main.dart
+++ lib/main.dart
@@ -9,11 +9,20 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+    return MaterialApp(
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSwatch(
+          backgroundColor: const Color(0xFFE7626C),
         ),
+        textTheme: const TextTheme(
+          displayLarge: TextStyle(
+            color: Color(0xFF232B55),
+          ),
+        ),
+        cardColor: const Color(0xFFF4EDDB),
+      ),
+      home: const Scaffold(
+        body: Center(),
       ),
     );
   }
