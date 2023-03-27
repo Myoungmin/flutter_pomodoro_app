e0308be2573ecd95cffd92781aa8f6cb411b3854
AzureAD\고명민 <miung222@gmail.com>
2023-03-28 07:58:34 +0900

    HomeScreen이라는 StatefulWidget 추가

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_pomodoro_app/screens/home_screen.dart';
 
 void main() {
   runApp(const MainApp());
@@ -21,9 +22,7 @@ class MainApp extends StatelessWidget {
         ),
         cardColor: const Color(0xFFF4EDDB),
       ),
-      home: const Scaffold(
-        body: Center(),
-      ),
+      home: const HomeScreen(),
     );
   }
 }
