0d6dbc0cfa9c7caceddf5b5065048f56562b24a7
AzureAD\고명민 <miung222@gmail.com>
2023-04-03 07:53:51 +0900

    Expanded widget으로 가로 전체로 확장하기
    container를 row로 감싸고 Expanded widget 사용

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -40,16 +40,22 @@ class _HomeScreenState extends State<HomeScreen> {
           ),
           Flexible(
             flex: 1,
-            child: Container(
-              decoration: BoxDecoration(
-                color: Theme.of(context).cardColor,
-              ),
-              child: Column(
-                children: const [
-                  Text('Pomodors'),
-                  Text('0'),
-                ],
-              ),
+            child: Row(
+              children: [
+                Expanded(
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: Theme.of(context).cardColor,
+                    ),
+                    child: Column(
+                      children: const [
+                        Text('Pomodors'),
+                        Text('0'),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
             ),
           ),
         ],
